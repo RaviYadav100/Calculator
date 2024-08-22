3a594819dc336971f771c56eaea7067313dbd274
RaviYadav100 <114805682+RaviYadav100@users.noreply.github.com>
2024-08-22 16:49:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './component/Calculator';
+
+function App() {
+  return (
+    <>
+    <Calculator></Calculator>
+    </>
+  ); 
+}
+
+export default App;
